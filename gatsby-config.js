b1d63fa1e95f30497c46b91056d78644733ecd28
Jose Albizures <albizures3601@gmail.com>
2020-03-02 22:12:23 +0300

    fix(gatsby): fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,81 +1,82 @@
 require(`dotenv`).config({
-  path: `.env`
+	path: `.env`,
 });
 
 module.exports = {
-  siteMetadata: {
-    siteTitle: `Jose Albizures`,
-    siteTitleAlt: `Jose Albizures - Web developer`,
-    siteDescription: "Jose Albizures - Web developer",
-    siteUrl: "http://albizures.com",
-    author: `@_albizures`,
-    siteLanguage: `en`
-  },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`
-          }
-        ],
-        externalLinks: [
-          {
-            name: `Github`,
-            url: `https://github.com/albizures`
-          },
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/_albizures`
-          },
-          {
-            name: `Instagram`,
-            url: `https://www.instagram.com/_albizures`
-          }
-        ]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-113899183-1"
-      }
-    },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Jose Albizures - Web developer`,
-        short_name: `albizures-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`
-          }
-        ]
-      }
-    },
-    {
-      resolve: "gatsby-plugin-i18n",
-      options: {
-        langKeyDefault: "en",
-        useLangKeyLayout: false
-      }
-    },
-    `gatsby-plugin-offline`
-    // `gatsby-plugin-webpack-bundle-analyser-v2`,
-  ]
+	siteMetadata: {
+		siteTitle: `Jose Albizures`,
+		siteTitleAlt: `Jose Albizures - Web developer`,
+		siteDescription: 'Jose Albizures - Web developer',
+		siteHeadline: 'Jose Albizures - Web developer',
+		siteUrl: 'http://albizures.com',
+		author: `@_albizures`,
+		siteLanguage: `en`,
+	},
+	plugins: [
+		{
+			resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+			options: {
+				navigation: [
+					{
+						title: `Blog`,
+						slug: `/blog`,
+					},
+				],
+				externalLinks: [
+					{
+						name: `Github`,
+						url: `https://github.com/albizures`,
+					},
+					{
+						name: `Twitter`,
+						url: `https://twitter.com/_albizures`,
+					},
+					{
+						name: `Instagram`,
+						url: `https://www.instagram.com/_albizures`,
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-113899183-1',
+			},
+		},
+		`gatsby-plugin-sitemap`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Jose Albizures - Web developer`,
+				short_name: `albizures-blog`,
+				description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+				start_url: `/`,
+				background_color: `#fff`,
+				theme_color: `#6B46C1`,
+				display: `standalone`,
+				icons: [
+					{
+						src: `/android-chrome-192x192.png`,
+						sizes: `192x192`,
+						type: `image/png`,
+					},
+					{
+						src: `/android-chrome-512x512.png`,
+						sizes: `512x512`,
+						type: `image/png`,
+					},
+				],
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-i18n',
+			options: {
+				langKeyDefault: 'en',
+				useLangKeyLayout: false,
+			},
+		},
+		`gatsby-plugin-offline`,
+		// `gatsby-plugin-webpack-bundle-analyser-v2`,
+	],
 };
