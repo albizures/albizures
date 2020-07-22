25110eda48d790f4a7dd5d6067ead8f6e23578a7
Jose Albizures <albizures3601@gmail.com>
2020-07-22 11:58:29 +0300

    feat(info): update info

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
 					},
 					{
 						name: `Twitter`,
-						url: `https://twitter.com/_albizures`,
+						url: `https://twitter.com/albzrs`,
 					},
 					{
 						name: `Instagram`,
