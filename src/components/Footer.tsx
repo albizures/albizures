769bd3253f1efc7485eea6ab09b7ad23546ff957
Jose Albizures <albizures3601@gmail.com>
2022-02-08 13:47:20 +0300

    add goat counter for analytics

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,11 @@ export const Footer = () => {
 			<p className="text-center text-gray-500 font-light mt-10 pb-6">
 				© 2021 by Jose Albizures. All rights reserved.
 			</p>
+			<script
+				data-goatcounter="https://albizures.goatcounter.com/count"
+				async
+				src="//gc.zgo.at/count.js"
+			></script>
 		</footer>
 	);
 };
